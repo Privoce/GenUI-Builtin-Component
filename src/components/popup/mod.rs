5ac5391fe5bcd070212a882a03a731eaa03006bc
syf20020816 <syf20020816@outlook.com>
2025-02-14 05:02:44 -0800

    popup getter setter

--- src/components/popup/mod.rs
+++ src/components/popup/mod.rs
@@ -2,14 +2,13 @@ pub mod register;
 
 use makepad_widgets::*;
 
-
 use crate::{
     shader::{
         draw_popup::DrawGPopup,
         manual::{CloseMode, PopupMode, Position},
     },
     themes::Themes,
-    utils::{BoolToF32, ThemeColor},
+    utils::{BoolToF32, ThemeColor, ToBool},
 };
 
 use super::view::GView;
@@ -35,44 +34,6 @@ pub struct GPopupContainer {
 impl LiveHook for GPopupContainer {
     fn after_apply(&mut self, cx: &mut Cx, apply: &mut Apply, index: usize, nodes: &[LiveNode]) {
         self.deref_widget.after_apply(cx, apply, index, nodes);
-        // if !self.visible {
-        //     return;
-        // }
-
-        // // ----------------- background color -------------------------------------------
-        // let bg_color = self.background_color.get(self.theme, 500);
-        // // ------------------ hover color -----------------------------------------------
-        // let hover_color = self.hover_color.get(self.theme, 400);
-        // // ------------------ focus color ---------------------------------------------
-        // let focus_color = self.focus_color.get(self.theme, 600);
-        // // ------------------ border color ----------------------------------------------
-        // let border_color = self.border_color.get(self.theme, 800);
-        // let shadow_color = self.shadow_color.get(self.theme, 700);
-        // // ------------------ is background_visible --------------------------------------------
-        // let background_visible = self.background_visible.to_f32();
-        // // ------------------ apply draw_popup --------------------------------------------
-        // let border_width = self.border_width;
-        // let border_radius = self.border_radius;
-        // let shadow_offset = self.shadow_offset;
-        // let spread_radius = self.spread_radius;
-        // let blur_radius = self.blur_radius;
-        // self.draw_view.apply_over(
-        //     cx,
-        //     live! {
-        //         background_color: (bg_color),
-        //         border_color: (border_color),
-        //         border_width: (border_width),
-        //         border_radius: (border_radius),
-        //         focus_color: (focus_color),
-        //         hover_color: (hover_color),
-        //         background_visible: (background_visible),
-        //         shadow_color: (shadow_color),
-        //         shadow_offset: (shadow_offset),
-        //         spread_radius: (spread_radius),
-        //         blur_radius: (blur_radius)
-        //     },
-        // );
-        // self.draw_view.redraw(cx);
     }
 }
 
@@ -160,10 +121,21 @@ pub struct GPopup {
 }
 
 impl LiveHook for GPopup {
-    fn after_apply(&mut self, cx: &mut Cx, _apply: &mut Apply, _index: usize, _nodes: &[LiveNode]) {
-        // if !self.visible {
-        //     return;
-        // }
+    fn after_apply(&mut self, cx: &mut Cx, apply: &mut Apply, index: usize, nodes: &[LiveNode]) {
+        self.draw_popup.after_apply(cx, apply, index, nodes)
+    }
+    fn after_apply_from_doc(&mut self, cx: &mut Cx) {
+        if !self.visible {
+            return;
+        }
+        if let Err(e) = self.render(cx) {
+            error!("GPopup render error: {:?}", e);
+        }
+    }
+}
+
+impl GPopup {
+    pub fn render(&mut self, cx: &mut Cx) -> Result<(), Box<dyn std::error::Error>> {
         // ----------------- background color -------------------------------------------
         let bg_color = self.background_color.get(self.theme, 500);
         let shadow_color = self.shadow_color.get(self.theme, 500);
@@ -193,11 +165,8 @@ impl LiveHook for GPopup {
                 blur_radius: (self.blur_radius)
             },
         );
-        // self.draw_popup.redraw(cx);
+        Ok(())
     }
-}
-
-impl GPopup {
     pub fn area(&self) -> Area {
         self.draw_popup.area()
     }
@@ -361,5 +330,292 @@ impl GPopup {
     }
     pub fn get_mut(&mut self) -> &mut GPopupContainer {
         &mut self.container
+    }
+    pub fn get_theme(&self) -> Themes {
+        self.theme
+    }
+    pub fn set_theme(
+        &mut self,
+        cx: &mut Cx,
+        theme: Themes,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.theme = theme;
+        self.render(cx)
+    }
+    pub fn get_opacity(&self) -> f32 {
+        self.opacity
+    }
+    pub fn set_opacity(
+        &mut self,
+        cx: &mut Cx,
+        opacity: f32,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.opacity = opacity;
+        self.render(cx)
+    }
+    pub fn get_background_color(&self) -> String {
+        crate::utils::vec4_to_hex(&self.draw_popup.background_color)
+    }
+    pub fn set_background_color(
+        &mut self,
+        cx: &mut Cx,
+        background_color: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.background_color
+            .replace(crate::utils::hex_to_vec4(&background_color)?);
+        self.render(cx)
+    }
+    pub fn get_hover_color(&self) -> String {
+        crate::utils::vec4_to_hex(&self.draw_popup.hover_color)
+    }
+    pub fn set_hover_color(
+        &mut self,
+        cx: &mut Cx,
+        hover_color: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.hover_color
+            .replace(crate::utils::hex_to_vec4(&hover_color)?);
+        self.render(cx)
+    }
+    pub fn get_focus_color(&self) -> String {
+        crate::utils::vec4_to_hex(&self.draw_popup.focus_color)
+    }
+    pub fn set_focus_color(
+        &mut self,
+        cx: &mut Cx,
+        focus_color: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.focus_color
+            .replace(crate::utils::hex_to_vec4(&focus_color)?);
+        self.render(cx)
+    }
+    pub fn get_border_color(&self) -> String {
+        crate::utils::vec4_to_hex(&self.draw_popup.border_color)
+    }
+    pub fn set_border_color(
+        &mut self,
+        cx: &mut Cx,
+        border_color: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.border_color
+            .replace(crate::utils::hex_to_vec4(&border_color)?);
+        self.render(cx)
+    }
+    pub fn get_border_width(&self) -> f32 {
+        self.draw_popup.border_width
+    }
+    pub fn set_border_width(
+        &mut self,
+        cx: &mut Cx,
+        border_width: f32,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.border_width = border_width;
+        self.render(cx)
+    }
+    pub fn get_border_radius(&self) -> f32 {
+        self.draw_popup.border_radius
+    }
+    pub fn set_border_radius(
+        &mut self,
+        cx: &mut Cx,
+        border_radius: f32,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.border_radius = border_radius;
+        self.render(cx)
+    }
+    pub fn get_visible(&self) -> bool {
+        self.visible
+    }
+    pub fn set_visible(
+        &mut self,
+        cx: &mut Cx,
+        visible: bool,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.visible = visible;
+        self.render(cx)
+    }
+    pub fn get_background_visible(&self) -> bool {
+        self.draw_popup.background_visible.to_bool()
+    }
+    pub fn set_background_visible(
+        &mut self,
+        cx: &mut Cx,
+        background_visible: bool,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.background_visible = background_visible;
+        self.render(cx)
+    }
+    pub fn get_cursor(&self) -> MouseCursor {
+        self.cursor.unwrap_or_default()
+    }
+    pub fn set_cursor(
+        &mut self,
+        cx: &mut Cx,
+        cursor: MouseCursor,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.cursor.replace(cursor);
+        self.render(cx)
+    }
+    pub fn get_mode(&self) -> PopupMode {
+        self.mode
+    }
+    pub fn set_mode(
+        &mut self,
+        cx: &mut Cx,
+        mode: PopupMode,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.mode = mode;
+        self.render(cx)
+    }
+    pub fn get_close_mode(&self) -> CloseMode {
+        self.close_mode
+    }
+    pub fn set_close_mode(
+        &mut self,
+        cx: &mut Cx,
+        close_mode: CloseMode,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.close_mode = close_mode;
+        self.render(cx)
+    }
+    pub fn get_shadow_color(&self) -> String {
+        crate::utils::vec4_to_hex(&self.draw_popup.shadow_color)
+    }
+    pub fn set_shadow_color(
+        &mut self,
+        cx: &mut Cx,
+        shadow_color: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.shadow_color
+            .replace(crate::utils::hex_to_vec4(&shadow_color)?);
+        self.render(cx)
+    }
+    pub fn get_spread_radius(&self) -> f32 {
+        self.draw_popup.spread_radius
+    }
+    pub fn set_spread_radius(
+        &mut self,
+        cx: &mut Cx,
+        spread_radius: f32,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.spread_radius = spread_radius;
+        self.render(cx)
+    }
+    pub fn get_blur_radius(&self) -> f32 {
+        self.draw_popup.blur_radius
+    }
+    pub fn set_blur_radius(
+        &mut self,
+        cx: &mut Cx,
+        blur_radius: f32,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.blur_radius = blur_radius;
+        self.render(cx)
+    }
+    pub fn get_shadow_offset(&self) -> Vec2 {
+        self.draw_popup.shadow_offset
+    }
+    pub fn set_shadow_offset(
+        &mut self,
+        cx: &mut Cx,
+        shadow_offset: Vec2,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.shadow_offset = shadow_offset;
+        self.render(cx)
+    }
+    pub fn get_height(&self) -> Size {
+        self.walk.height
+    }
+    pub fn set_height(
+        &mut self,
+        cx: &mut Cx,
+        height: Size,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.walk.height = height;
+        self.render(cx)
+    }
+    pub fn get_width(&self) -> Size {
+        self.walk.width
+    }
+    pub fn set_width(
+        &mut self,
+        cx: &mut Cx,
+        width: Size,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.walk.width = width;
+        self.render(cx)
+    }
+    pub fn get_margin(&self) -> Margin {
+        self.walk.margin
+    }
+    pub fn set_margin(
+        &mut self,
+        cx: &mut Cx,
+        margin: Margin,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.walk.margin = margin;
+        self.render(cx)
+    }
+    pub fn get_abs_pos(&self) -> Option<DVec2> {
+        self.walk.abs_pos
+    }
+    pub fn set_abs_pos(
+        &mut self,
+        cx: &mut Cx,
+        abs_pos: Option<DVec2>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.walk.abs_pos = abs_pos;
+        self.render(cx)
+    }
+    pub fn get_padding(&self) -> Padding {
+        self.layout.padding
+    }
+    pub fn set_padding(
+        &mut self,
+        cx: &mut Cx,
+        padding: Padding,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.layout.padding = padding;
+        self.render(cx)
+    }
+    pub fn get_flow(&self) -> Flow {
+        self.layout.flow
+    }
+    pub fn set_flow(&mut self, cx: &mut Cx, flow: Flow) -> Result<(), Box<dyn std::error::Error>> {
+        self.layout.flow = flow;
+        self.render(cx)
+    }
+    pub fn get_scroll(&self) -> DVec2 {
+        self.layout.scroll
+    }
+    pub fn set_scroll(
+        &mut self,
+        cx: &mut Cx,
+        scroll: DVec2,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.layout.scroll = scroll;
+        self.render(cx)
+    }
+    pub fn get_align(&self) -> Align {
+        self.layout.align
+    }
+    pub fn set_align(
+        &mut self,
+        cx: &mut Cx,
+        align: Align,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.layout.align = align;
+        self.render(cx)
+    }
+    pub fn get_spacing(&self) -> f64 {
+        self.layout.spacing
+    }
+    pub fn set_spacing(
+        &mut self,
+        cx: &mut Cx,
+        spacing: f64,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.layout.spacing = spacing;
+        self.render(cx)
     }
 }
